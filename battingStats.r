4b54b0b247273337c2463ec14a0e2e3d6600f8fc
Mark Borcherding <markborcherding@gmail.com>
2018-04-20 06:53:25 -0500

    try adding mlb_id

--- battingStats.r
+++ battingStats.r
@@ -16,20 +16,34 @@ battingStats <- function(startDate, endDate){
       paste(
         startDate,
         endDate,
-        sep = "."
-      ),
+        sep = "."),
       "level=mlb",
       "franch=",
       "stat=",
       "stat_value=0",
-      sep = "&"
-    ),
-    sep = ""
-  )
+      sep = "&"),
+    sep = "")
 
-  rawTable <-
+  extractMLB_ID <- function(node, encoding) {
+    stat <- xmlGetAttr(node, "data-stat")
+    val <- xmlValue(node)
+    td <- xmlName(node) == "td"
+    if(td & stat == "player"){
+      #mlb_id <- gsub(".*mlb_ID=", "", xmlGetAttr(node, "data-append-csv"))
+      paste(
+        val,
+        "mlb_id",
+        sep = "::")
+    } else {
+      as.character(val)
+    }
+  }
+
+  table <-
     readHTMLTable(
-      getURL(url, .opts = list(followlocation = TRUE)))
+      getURL(url, .opts = list(followlocation = TRUE)),
+      elFun = extractMLB_ID,
+      )$daily
 
-  return (url)
+  return (table[table$RBI != "RBI", ])
 }
